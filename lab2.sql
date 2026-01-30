0b266dc30aed6ac234d1abc61cb8cb6dff2cf95e
bibek <bibek@khatri.com>
2026-01-30 08:52:11 +0545

    LoanManagement.vpp

---
+++ lab2.sql
@@ -0,0 +1,68 @@
+#1.	Create a database named dbemp and switch to it.
+create database dbemp1;
+use dbemp1;
+#2.	Create an employee table with appropriate data 
+#types and constraints for employee details.
+Create table employee (
+	EmployeeID varchar(10),
+    FirstName varchar(20),
+    LastName varchar(20),
+    Gender char(1),
+    DateofBirth date,
+    Designation varchar(20),
+    DepartmentName varchar(20),
+    ManagerId varchar(10),
+    JoinedDate date,
+    Salary decimal(10,0)
+);
+#3.	Insert at least two employee records into the employee table.
+Insert into employee(
+EmployeeID ,FirstName ,LastName ,Gender ,DateofBirth ,
+Designation ,DepartmentName ,ManagerId ,JoinedDate ,Salary 
+) values(
+'001','Season','Maharjan','M','1996-04-02','Engineer',
+'Software engineering','002','2022-11-02','5000000000'
+),(
+'003','Srijana','Maharjan','F','2000-04-02','Manager',
+'Software engineering','005','2025-11-02','9000000');
+
+select * from employee;
+
+
+
+
+#4. Update the gender of an employee whose #EmployeeID is 003.
+upadte employee
+set Gender = 'M'
+where EmployeeID = '003';
+
+
+#5. Display the first name, current date, date of birth, and age of employees who 
+#are older than 20 years.
+
+SELECT FirstName,CURADTE() as DateofBirth from employee;
+DateofBirth,
+timestampdiff(YEAR,DateofBirth,CURDATE()) as age
+from employee
+where
+timestampdiff(YEAR,DateofBirth,CURRENT()) > 20;
+
+#6. Write a query to find the oldest employee
+select Min(DateofBirth) from employee;
+select * from employee
+where DateofBirth = ();
+
+#7. Write a query to find the youngest employee
+select Min(Age) from empployee;
+select * from employee
+where DateofBirth = ();
+
+#8. Display the maximum salary department-wise.
+select DepartmentName, Max(Salary) as Maxsalary 
+from employee Group By DepartmentName;
+
+#9. Display the minimum salary department-wise.
+select DepartmentName, Min(Salary) as Minsalary 
+from employee Group By DepartmentName;
+#10. List the employee who act as manager.
+select  *from employee ;
